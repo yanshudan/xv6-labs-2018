f4bb0d74976f9f6e777b8c9bbc5493a62857c7cc
dsd <jhq13@foxmailcom>
2022-01-03 15:55:30 +0800

    add backtrace command, remove some cprintf

--- kern/monitor.c
+++ kern/monitor.c
@@ -24,6 +24,7 @@ struct Command
 static struct Command commands[] = {
 	{"help", "Display this list of commands", mon_help},
 	{"kerninfo", "Display information about the kernel", mon_kerninfo},
+	{"backtrace", "Show back trace infomation on stack(kern only)", mon_backtrace}
 };
 
 /***** Implementations of basic kernel monitor commands *****/
